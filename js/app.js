e5ae4923291f95874495f64a48f9b963fc237e65
longfengfan <longfengfan@travelsky.com.cn>
2022-11-21 16:55:31 +0800

    first

--- js/app.js
+++ js/app.js
@@ -3,4 +3,48 @@
 
 	// Your starting point. Enjoy the ride!
 
+	new Vue({
+		el: "#mainApp",
+		data: {
+			todos: [
+				{ id: 1, completed: true, title: "实力内容1" },
+				{ id: 2, completed: true, title: "实力内容2" },
+				{ id: 3, completed: false, title: "实力内容3" }
+			],
+			inputStr: ''
+		},
+		computed: {
+			remainting() {
+				return this.todos.filter(o => !o.completed).length;
+			}
+		},
+		methods: {
+			populateS(value) {
+				return value + (this.remainting > 1 ? 's' : '');
+			},
+			toggleAll() {
+				if(this.remainting) {
+					for(var o of this.todos) {
+						o.completed = true;
+					}
+				}else{
+					for(var o of this.todos) {
+						o.completed = false;
+					}
+				}
+			},
+			inputBlur() {
+				if(this.inputStr != null && this.inputStr.length > 0) {
+					var max = 0;
+					for(var o of this.todos) {
+						max = max > o.id ? max : o.id;
+					}
+					var newTodo = {id: ++max, completed: false, title: this.inputStr}
+					this.inputStr='';
+					this.todos.push(newTodo)
+				}
+			}
+		}
+	});
+
 })(window);
